5f13be802458cc1f47e821f8d0e5879cf4f0ba44
Demo User <demouser@MacBook-Pro.local>
2018-07-17 13:30:51 -0500

    java initial

---
+++ java/javascript.js
@@ -0,0 +1,13 @@
+console.log("Hello World" );
+const name = 'Charlie';
+console.log('Hello World ' + name);
+const age = 16;
+console.log(`You are ${age + 1 } years old`);
+if(age)
+{
+ console.log("true");
+}
+else
+{
+console.log("false");
+}
